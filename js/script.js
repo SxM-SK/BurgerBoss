09798800fee611fa52a56d147874ddd69b2d33e5
Vaso13 <92309598+Vaso13@users.noreply.github.com>
2021-12-21 19:36:20 +0400

    BurgerBoss

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+var toggleButton = document.querySelector('.toggle-menu');
+var navBar = document.querySelector('.nav-bar');
+toggleButton.addEventListener('click', function () {
+	navBar.classList.toggle('toggle');
+}); 
